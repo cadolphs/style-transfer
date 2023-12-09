dfee6ea94fc9df761cee0aa2dd7736d620a74808
Clemens Adolphs <clemens.adolphs@gmail.com>
2023-12-09 13:02:27 -0800

    Get ldm and main module imports to work too

--- simple_script.py
+++ simple_script.py
@@ -2,6 +2,9 @@
 
 from modal import Stub, Image
 
+ARTFUSION_GITHUB_PATH = "https://github.com/cadolphs/ArtFusion.git"
+ARTFUSION_PATH = "/git/artfusion/"
+
 stub = Stub("art-fusion")
 
 image = (
@@ -19,6 +22,11 @@ image = (
         "imageio-ffmpeg",
         "albumentations",
     )
+    .apt_install("git")
+    .run_commands(
+        f"cd / && mkdir -p {ARTFUSION_PATH} && cd {ARTFUSION_PATH} && git clone --depth 1 {ARTFUSION_GITHUB_PATH} .",
+        force_build=False,
+    )
 )
 
 
@@ -35,6 +43,10 @@ def test_imports():
     from omegaconf import OmegaConf
     import albumentations
 
-    #    from ldm.models.diffusion.ddim import DDIMSampler
+    import sys
+
+    sys.path.append(ARTFUSION_PATH)
+    from main import instantiate_from_config
+    from ldm.models.diffusion.ddim import DDIMSampler
 
     return True
