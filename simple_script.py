2778464034a3e560e0358dc4222e724f1b718bd5
Clemens Adolphs <clemens.adolphs@gmail.com>
2023-12-11 12:52:48 -0800

    Bake pretrained model into the image

--- simple_script.py
+++ simple_script.py
@@ -1,11 +1,28 @@
 # Starting with a clean slate
 
-from modal import Stub, Image
+from modal import Secret, Stub, Image
 
 ARTFUSION_GITHUB_PATH = "https://github.com/cadolphs/ArtFusion.git"
 ARTFUSION_PATH = "/git/artfusion/"
+MODEL_DIR = "/models"
+MODEL_NAME = "artfusion_r12_step=317673.ckpt"
+BASE_MODEL = "lagerbaer/artfusion"
 
 stub = Stub("art-fusion")
+
+
+def download_model_to_folder():
+    from huggingface_hub import snapshot_download
+    import os
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    snapshot_download(
+        BASE_MODEL,
+        local_dir=MODEL_DIR,
+        token=os.environ["HUGGINGFACE_TOKEN"],
+    )
+
 
 image = (
     Image.debian_slim()
@@ -21,11 +38,16 @@ image = (
         "imageio",
         "imageio-ffmpeg",
         "albumentations",
+        "hf-transfer~=0.1",
     )
     .apt_install("git")
     .run_commands(
         f"cd / && mkdir -p {ARTFUSION_PATH} && cd {ARTFUSION_PATH} && git clone --depth 1 {ARTFUSION_GITHUB_PATH} .",
         force_build=False,
+    )
+    .run_commands(f"cd / && mkdir -p {MODEL_DIR}")
+    .run_function(
+        download_model_to_folder, secret=Secret.from_name("my-huggingface-secret")
     )
 )
 
@@ -49,4 +71,8 @@ def test_imports():
     from main import instantiate_from_config
     from ldm.models.diffusion.ddim import DDIMSampler
 
+    import os
+    import pathlib
+
+    assert pathlib.Path(MODEL_DIR, MODEL_NAME).exists()
     return True
